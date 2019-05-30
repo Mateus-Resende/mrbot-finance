b86311b8d36d88b1d6c83523d2670f66b919720a
Mateus Resende <mateus.resende@runtastic.com>
2019-05-30 18:44:19 +0200

    Add rollbar

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
 const ngrok = require('ngrok')
+const Rollbar = require('rollbar')
 
 const HelpController = require('./use-case/help/help')
 
@@ -10,8 +11,15 @@ const token = process.env.TELEGRAM_BOT_TOKEN
 const env = process.env.ENVIRONMENT
 const bot = new Telegraf(token)
 
+const rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true
+})
+
 const start = function (url, port, bot) {
   bot.command('help', ({ reply }) => {
+    rollbar.log('help command')
     reply(HelpController.getCommands(), Extra.HTML())
   })
 
@@ -20,6 +28,7 @@ const start = function (url, port, bot) {
   })
 
   bot.catch((err) => {
+    rollbar.error(err)
     console.log(err)
   })
 
@@ -31,12 +40,17 @@ const start = function (url, port, bot) {
   })
 }
 
+rollbar.log(process.env.ENVIRONMENT)
+
 if (env === 'production' || env === 'staging') {
+  rollbar.log('Hello world!')
+  rollbar.log(process.env.URL)
   start(process.env.URL, process.env.PORT, bot)
 } else {
   ngrok
     .connect(process.env.PORT)
     .then((url) => {
+      rollbar.log(url)
       start(url, process.env.PORT, bot)
     })
     .catch(e => console.error(e))
